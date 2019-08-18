f17f3c3deae47c87101ebcfbebe89dd8ec77c32b
janakiram <janakiram.chava@outlook.com>
2019-08-18 19:14:24 +0530

    Added dynamic nature to script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'CSS Dynamic update';
+  marks:number = 71;
+  mycolor:string = "";
+  constructor() {
+    if (this.marks >= 35) {
+      this.mycolor = "green";
+    } else {
+      this.mycolor = "red";
+    }
+  }
+    changeColor(){
+    //console.debug('marks: ',this.marks);
+      if(this.marks>=35){
+        this.mycolor="green";
+      }else {
+        this.mycolor="red";
+      }
+    }
+
+
+
 }
